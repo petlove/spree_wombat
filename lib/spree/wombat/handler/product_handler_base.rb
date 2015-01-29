72cd6dd7562fdf7b9d1ea0bbc521ccf2e97d3732
joao paulo <jpcamargo+github@gmail.com>
2015-01-29 16:40:27 -0200

    fixes the unscoped issue finding variantes outside of the product scope

--- lib/spree/wombat/handler/product_handler_base.rb
+++ lib/spree/wombat/handler/product_handler_base.rb
@@ -134,13 +134,10 @@ module Spree
             child_product[:options] = option_type_values.collect {|k,v| {name: k, value: v} }
             child_product[:price] = price
 
-            variant = product.variants.unscoped.find_by_sku(child_product[:sku])
+            variant = Spree::Variant.unscoped.where(sku: child_product[:sku], product: product).first_or_initialize
 
-            if variant
-              variant.update_attributes(child_product)
-            else
-              variant = product.variants.create!({ product: product }.merge(child_product))
-            end
+            variant.update_attributes!(child_product)
+
             process_images(variant, images)
           end
         end
