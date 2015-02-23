a7014104bee4ef603cbeab47dbbafe4b7941bbb6
joao paulo <jpcamargo+github@gmail.com>
2015-02-23 19:49:57 -0300

    changes set_price_handler column to vtex_sku_id

--- lib/spree/wombat/handler/set_price_handler.rb
+++ lib/spree/wombat/handler/set_price_handler.rb
@@ -4,8 +4,8 @@ module Spree
       class SetPriceHandler < Base
         def process
           sku = @payload[:price].delete(:product_id)
-          variant = Spree::Variant.find_by_sku(sku)
-          return response("Product with SKU #{sku} was not found", 500) unless variant
+          variant = Spree::Variant.find_by(vtex_sku_id: sku)
+          return response("Product with VTEX SKU ID #{sku} was not found", 500) unless variant
 
           updatable_columns = @payload[:price].slice *Spree::Variant.attribute_names.select{|n| n =~ /price/i }.concat(["price"])
           return response("Missing price information", 500) unless updatable_columns[:price]
@@ -16,8 +16,9 @@ module Spree
           Spree::Variant.transaction do
             variant.update_attributes!(updatable_columns)
           end
+          variant.reload
 
-          return response("Set price for #{sku} from #{current_price} #{current_currency} to #{variant.reload.price} #{variant.reload.currency}")
+          return response("Set price for #{sku} from #{current_price} #{current_currency} to #{variant.price} #{variant.currency}")
         end
       end
     end
