9301583cc1e114dc2d017eac417f625b1c6f68e4
Rodrigo <rodrigo@vostro.(none)>
2015-03-17 17:35:22 -0300

    adding cpf_or_cnpj attribute to order serializer

--- app/serializers/spree/wombat/order_serializer.rb
+++ app/serializers/spree/wombat/order_serializer.rb
@@ -5,7 +5,7 @@ module Spree
     class OrderSerializer < ActiveModel::Serializer
 
       attributes :id, :status, :channel, :email, :currency, :placed_on, :updated_at, :totals,
-        :adjustments, :guest_token, :shipping_instructions
+        :adjustments, :guest_token, :shipping_instructions, :cpf_or_cnpj
 
       has_many :line_items,  serializer: Spree::Wombat::LineItemSerializer
       has_many :payments, serializer: Spree::Wombat::PaymentSerializer
@@ -19,6 +19,10 @@ module Spree
 
       def shipping_instructions
         object.special_instructions
+      end
+
+      def cpf_or_cnpj
+        object.payments.last.security_number
       end
 
       def status
