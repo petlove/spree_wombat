c45d40afb35d80eebb534a379f5db568ed4a3f17
Rodrigo Vitulli <rovitulli@gmail.com>
2015-05-26 13:45:27 -0300

    add method to serialize image small url of line item

--- app/serializers/spree/wombat/line_item_serializer.rb
+++ app/serializers/spree/wombat/line_item_serializer.rb
@@ -3,7 +3,8 @@ require 'active_model/serializer'
 module Spree
   module Wombat
     class LineItemSerializer < ActiveModel::Serializer
-      attributes :id, :product_id, :name, :quantity, :price, :weight, :height, :width, :depth, :promotional_item
+      attributes :id, :product_id, :name, :quantity, :price, :weight, :height, :width, :depth,
+      :promotional_item, :image_small_url, :product_url
 
       def product_id
         object.variant.sku
@@ -31,6 +32,14 @@ module Spree
 
       def depth
         object.variant.depth
+      end
+
+      def image_small_url
+        (object.variant.images + object.variant.product.images).first.try(:attachment,:small)
+      end
+
+      def product_url
+        "http://www.petlove.com.br/%s/p" % object.variant.product.slug
       end
 
     end
