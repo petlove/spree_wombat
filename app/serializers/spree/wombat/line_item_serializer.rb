6553fdfd50a4ff8c633df854d68e890312c4783a
joao paulo <jpcamargo+github@gmail.com>
2015-06-02 17:44:01 -0300

    line item serializer sets dimention stats as floats

--- app/serializers/spree/wombat/line_item_serializer.rb
+++ app/serializers/spree/wombat/line_item_serializer.rb
@@ -10,28 +10,36 @@ module Spree
         object.variant.sku
       end
 
+      def name
+        object.name
+      end
+
+      def quantity
+        object.respond_to?(:quantity) ? object.quantity.to_i : 1
+      end
+
       def price
         object.price.to_f
       end
 
-      def promotional_item
-        object.promotional_item
-      end
-
       def weight
-        object.variant.weight
+        object.variant.weight.round(4).to_f
       end
 
       def height
-        object.variant.height
+        object.variant.height.round(4).to_f
       end
 
       def width
-        object.variant.width
+        object.variant.width.round(4).to_f
       end
 
       def depth
-        object.variant.depth
+        object.variant.depth.round(4).to_f
+      end
+
+      def promotional_item
+        object.promotional_item
       end
 
       def image_url
