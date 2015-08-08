6188909642033be91afe01c4de38756b7ba8ca3e
joao paulo <jpcamargo+github@gmail.com>
2015-08-08 14:09:10 -0300

    adds a validation, that checks for the payload's order's status

--- lib/spree/wombat/handler/cancel_order_handler.rb
+++ lib/spree/wombat/handler/cancel_order_handler.rb
@@ -4,15 +4,17 @@ module Spree
       class CancelOrderHandler < OrderHandlerBase
 
         def process
+          return response("Invalid status: #{@payload[:order][:status]}", 500) unless @payload[:order][:status] == 'erp_canceled'
+
           order_number = @payload[:order][:id]
           order = Spree::Order.lock(true).find_by(number: order_number)
           return response("Order with number #{order_number} was not found", 500) unless order
           return response("Order already canceled", 500) if order.canceled?
-          
+
           if order.cancel
             response "Canceled Order with number #{order_number}"
           else
-            response("Order with number #{order_number} could not be canceled: #{order.errors.full_messages.join("; ")}", 500)
+            response("Order with number #{order_number}, with state #{order.state}, could not be canceled: #{order.errors.full_messages.join("; ")}", 500)
           end
         end
       end
--- spec/lib/spree/wombat/handler/cancel_order_handler_spec.rb
+++ spec/lib/spree/wombat/handler/cancel_order_handler_spec.rb
@@ -9,36 +9,50 @@ module Spree
         let!(:message) { ::Hub::Samples::Order.request }
         let(:handler) { Handler::CancelOrderHandler.new(message.to_json) }
 
-        context "for existing completed order" do
-          let!(:order) { create(:completed_order_with_totals, number: message["order"]["id"]) }
-
-          it "will cancel the order" do
-            responder = handler.process
-            expect(responder.summary).to  match /Canceled Order with number R.{9}/
-            expect(responder.code).to eql 200
-
-            expect(order.reload.state).to eql 'canceled'
-          end
-        end
-
-        context "for existing incomplete order" do
-          let!(:order) { create(:order_with_line_items, number: message["order"]["id"]) }
-
-          it "will cancel the order" do
-            responder = handler.process
-            expect(responder.summary).to  match /Order with number R.{9} could not be canceled/
-            expect(responder.code).to eql 500
-          end
-        end
-
-        context "with no order present" do
-
+        context "with invalid status" do
           it "returns a Wombat::Responder with 500 status" do
             responder = handler.process
-            expect(responder.summary).to match /Order with number R.{9} was not found/
+            expect(responder.summary).to match /Invalid status/
             expect(responder.code).to eql 500
           end
+        end
 
+        context "with a valid status" do
+          let!(:message) do
+            msg = ::Hub::Samples::Order.request
+            msg["order"].merge!("status" => "erp_canceled")
+            msg
+          end
+
+          context "with no order present" do
+            it "returns a Wombat::Responder with 500 status" do
+              responder = handler.process
+              expect(responder.summary).to match /Order with number R.{9} was not found/
+              expect(responder.code).to eql 500
+            end
+          end
+
+          context "for existing incomplete order" do
+            let!(:order) { create(:order_with_line_items, number: message["order"]["id"]) }
+
+            it "returns a Wombat::Responder with 500 status" do
+              responder = handler.process
+              expect(responder.summary).to  match /Order with number R.{9}, with state \w+, could not be canceled/
+              expect(responder.code).to eql 500
+            end
+          end
+
+          context "for existing completed order" do
+            let!(:order) { create(:completed_order_with_totals, number: message["order"]["id"]) }
+
+            it "will cancel the order" do
+              responder = handler.process
+              expect(responder.summary).to  match /Canceled Order with number R.{9}/
+              expect(responder.code).to eql 200
+
+              expect(order.reload.state).to eql 'canceled'
+            end
+          end
         end
       end
 
