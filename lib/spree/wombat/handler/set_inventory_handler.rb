2da81c8c352a7882ca03903b9bf271c9e8910ddf
joao paulo <jpcamargo+github@gmail.com>
2015-10-20 18:20:46 -0200

    fixes an issue where the quantity would be a string

--- lib/spree/wombat/handler/set_inventory_handler.rb
+++ lib/spree/wombat/handler/set_inventory_handler.rb
@@ -7,6 +7,7 @@ module Spree
           stock_location_name = @payload[:inventory][:location]
           subscription_stock_location_name = "#{stock_location_name}_subscription"
           sku = @payload[:inventory][:product_id]
+          quantity = @payload[:inventory][:quantity].to_i
 
           stock_location = Spree::StockLocation.find_by(name: stock_location_name) || Spree::StockLocation.find_by(admin_name: stock_location_name)
           return response("Stock location with name #{stock_location_name} was not found", 500) unless stock_location
@@ -22,10 +23,10 @@ module Spree
           count_on_hand = stock_item.count_on_hand
           subscription_count_on_hand = subscription_stock_item.try(:count_on_hand).to_i
 
-          stock_item.set_count_on_hand(@payload[:inventory][:quantity] - subscription_count_on_hand)
+          stock_item.set_count_on_hand(quantity - subscription_count_on_hand)
 
           msg = "Set inventory for #{sku} at #{stock_location_name} from #{count_on_hand} to "
-          msg += "#{@payload[:inventory][:quantity]} - #{subscription_count_on_hand} = " if subscription_stock_location && subscription_stock_item
+          msg += "#{quantity} - #{subscription_count_on_hand} = " if subscription_stock_location && subscription_stock_item
           msg += "#{stock_item.reload.count_on_hand}"
 
           response(msg)
